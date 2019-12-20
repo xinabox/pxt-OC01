2baf3459485d688d0a5fa52ffe08262ae52fa87e
KalbeAbbas <kalbeabbas142@gmail.com>
2019-12-20 18:32:32 +0500

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,14 +1,6 @@
  //% color=190 weight=100 icon="\uf1e6"
  namespace OC01
  {
-     export enum state
-     {
-         //% block=ON
-         ON = 0,
-         //% block=OFF
-         OFF = 1
-     }
-
     export enum oc01_pins
      {
          //% block=OUT0
@@ -34,16 +26,17 @@
      pins.i2cWriteNumber(PCA9536_I2C_ADDRESS, PCA9536_ALL_OUTPUTS_OFF, NumberFormat.UInt8BE) //value
 
      //% block="OC01 turn pin %pin| %pin_state|"
-     export function toggleOutput(pin: oc01_pins, pin_state: state)
+     //% pin_state.shadow="toggleOnOff"
+     export function toggleOutput(pin: oc01_pins, pin_state: boolean)
      {
          let current_pin_state: number
          current_pin_state = pins.i2cReadNumber(PCA9536_I2C_ADDRESS, NumberFormat.UInt8BE);
-         if(pin_state == state.ON)
+         if(pin_state == true)
          {
              current_pin_state |= (current_pin_state | pin)
              pins.i2cWriteNumber(PCA9536_I2C_ADDRESS, PCA9536_REG_OUTPUT_PORT,NumberFormat.UInt8BE) //reg
              pins.i2cWriteNumber(PCA9536_I2C_ADDRESS, current_pin_state, NumberFormat.UInt8BE) //value
-         }else if(pin_state == state.OFF)
+         }else if(pin_state == false)
          {
              current_pin_state &= ~(1 << current_pin_state | pin);
              pins.i2cWriteNumber(PCA9536_I2C_ADDRESS, PCA9536_REG_OUTPUT_PORT, NumberFormat.UInt8BE) //reg
